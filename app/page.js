f445240efe25379e61813e2ba085d08ba9a879cb
raymond <raymond.1zou@gmail.com>
2024-03-27 15:49:31 -0700

    add resume

--- app/page.js
+++ app/page.js
@@ -1,34 +1,34 @@
 import Nav from '../components/Nav';
 
 export default function Homepage() {
-  return <>
-    <Nav />
-    <div className="m-12 md:mx-48 md:w-1/2">
-      <div className="flex justify-center md:justify-start">
-        <img src="raymond-profile.jfif" className="rounded-full h-48" />
-      </div>
-      <br />
-      <p className="text-white text-lg">Hi I'm Raymond! I'm a 4th year student studying at the University of British Columbia,
-        currently completing a Combined Major in Science in Computer Science, Life Science and Earth & Environemntal Science. I am
-        a highly passionate fullstack web developer who enjoys transforming ideas into solutions for the ongoing challenges
-        presented by the digital world.
-      </p>
-      <br />
-      <p className="text-white text-lg">
-        Some of my hobbies include developing video games, cooking and exploring some of the great hiking spots British Columbia has to offer.
-      </p>
-      <br />
-      <p className="text-white text-lg">
-        Have a cool project idea, or just want to say hello? I'm always open to connect! You can reach me at
-        <a href="mailto:raymond.zf.zou@gmail.com" className="text-green hover:text-darkgreen"> raymond.zf.zou@gmail.com </a>
-        or 778-988-9076.
-      </p>
-    </div>
-  </>
+    return <>
+        <Nav />
+        <div className="m-12 md:mx-48 md:w-1/2">
+            <div className="flex justify-center md:justify-start">
+                <img src="raymond-profile.jfif" className="rounded-full h-48" />
+            </div>
+            <br />
+            <p className="text-white text-lg">Hi I'm Raymond! I'm a 5th year student studying at the University of British Columbia,
+                currently completing a Combined Major in Science in Computer Science, Life Science and Earth & Environemntal Science. I am
+                a highly passionate fullstack web developer who enjoys transforming ideas into solutions for the ongoing challenges
+                presented by the digital world.
+            </p>
+            <br />
+            <p className="text-white text-lg">
+                Some of my hobbies include developing video games, cooking and exploring some of the great hiking spots British Columbia has to offer.
+            </p>
+            <br />
+            <p className="text-white text-lg">
+                Have a cool project idea, or just want to say hello? I'm always open to connect! You can reach me at
+                <a href="mailto:raymond.zf.zou@gmail.com" className="text-green hover:text-darkgreen"> raymond.zf.zou@gmail.com </a>
+                or 778-988-9076. My resume can also be found <a href="/Raymond_Zou_Resume.pdf" className="text-green hover:text-darkgreen">here.</a>
+            </p>
+        </div>
+    </>
 }
 
 export const metadata = {
-  title: 'Raymond Zou - Web Developer Portfolio',
-  description: `Welcome to Raymond Zou's personal website. Explore some of my recent projects and learn more about the path
+    title: 'Raymond Zou - Web Developer Portfolio',
+    description: `Welcome to Raymond Zou's personal website. Explore some of my recent projects and learn more about the path
   I'm taking to become a web developer.`
 }
