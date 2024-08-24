e480f83e60194a296bf93df5308cca663538c740
RaymoZou <raymond.1zou@gmail.com>
2024-08-24 16:03:26 -0700

    extend tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      black: '#191A19',
-      grey: '#0F0F0F',
-      white: '#f8fafc',
-      darkgreen: '#1E5128',
-      green: '#4E9F3D',
-      lightgreen: '#D8E9A8'
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
 
-    }
-  },
-  plugins: [],
+            colors: {
+                black: '#191A19',
+                grey: '#0F0F0F',
+                white: '#f8fafc',
+                darkgreen: '#1E5128',
+                green: '#4E9F3D',
+                lightgreen: '#D8E9A8'
+
+            }
+        },
+    },
+    plugins: [],
 }
