be50c54cf82797f6d37370cee203b7b7efe963c8
RaymoZou <raymond.1zou@gmail.com>
2024-08-23 21:54:51 -0700

    update profile picture

--- app/page.js
+++ app/page.js
@@ -3,32 +3,38 @@ import Nav from '../components/Nav';
 export default function Homepage() {
     return <>
         <Nav />
-        <div className="m-12 md:mx-48 md:w-1/2">
-            <div className="flex justify-center md:justify-start">
-                <img src="raymond-profile.jfif" className="rounded-full h-48" />
+        {/* intro */}
+        <div className="my-12 mx-12 md:mx-48 xl:mx-96">
+            <p className="text-white font-bold text-4xl my-12">
+                raymond
+            </p>
+            <div className="flex justify-center">
+                <img src="profile_picture.jpg" className="rounded-full lg:h-96 lg:w-96 h-72 w-72 object-cover" />
             </div>
             <br />
-            <p className="text-white text-lg">Hi I'm Raymond! I'm a UBC graduate in the Combined Major in Science program with a focus
+            <p className="text-white text-lg">I'm a UBC grad in the Combined Major in Science program with a focus
                 in Computer Science, Life Science and Earth & Environemntal Science. I am
                 a highly passionate fullstack web developer who enjoys transforming ideas into solutions for the ongoing challenges
                 presented by the digital world. My current interest is in graphics programming with graphics APIs such OpenGL.
             </p>
             <br />
             <p className="text-white text-lg">
-                Some of my hobbies include developing video games, cooking and exploring some of the great hiking spots British Columbia has to offer.
+                In my spare time I like to make video games, run around the city, or spend hours trying to exit the Vim editor.
             </p>
             <br />
             <p className="text-white text-lg">
-                Have a cool project idea, or just want to say hello? I'm always open to connect! You can reach me at
+                Have a cool idea, or just want to say hello? You can reach me at
                 <a href="mailto:raymond.zf.zou@gmail.com" className="text-green hover:text-darkgreen"> raymond.zf.zou@gmail.com </a>
-                or 778-988-9076. My resume can also be found <a href="/Raymond_Zou_Resume.pdf" className="text-green hover:text-darkgreen">here.</a>
+                or 778-988-9076. You can also check out my resume <a href="/Raymond_Zou_Resume.pdf" className="text-green hover:text-darkgreen">here.</a>
             </p>
+            <hr className="w-1/2 mx-auto border-t-4 my-12" />
+            {/* projects */}
+
         </div>
     </>
 }
 
 export const metadata = {
-    title: 'Raymond Zou - Web Developer Portfolio',
-    description: `Welcome to Raymond Zou's personal website. Explore some of my recent projects and learn more about the path
-  I'm taking to become a web developer.`
+    title: 'Raymond Zou',
+    description: `Welcome to Raymond Zou's personal website. Come check out some of the cool stuff I'm working on!`
 }
