88febd5789a2b0d228b1d9fd57773979f3b7e3e4
RaymoZou <raymond.1zou@gmail.com>
2025-01-10 22:57:12 -0800

    add scroll smoothing page navigation

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className="bg-black text-white flex flex-col min-h-screen justify-between">
         <div>{children}</div>
       </body>
