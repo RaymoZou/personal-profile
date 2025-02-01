3f7dc2effac5b3111914cd7014e9294ebe811b37
RaymoZou <raymond.1zou@gmail.com>
2025-02-01 11:19:21 -0800

    add basic Vercel web analytics

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({ children }) {
 
-  return (
-    <html lang="en" className="scroll-smooth">
-      <body className="bg-black text-white flex flex-col min-h-screen justify-between">
-        <div>{children}</div>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className="scroll-smooth">
+            <body className="bg-black text-white flex flex-col min-h-screen justify-between">
+                <div>{children}</div>
+                <Analytics />
+            </body>
+        </html>
+    )
 }
