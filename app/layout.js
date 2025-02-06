06cc13c13eca1696ad79aff2647fc1d853dccb29
RaymoZou <raymond.1zou@gmail.com>
2025-02-05 22:08:25 -0800

    add Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }) {
 
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
             <body className="bg-black text-white flex flex-col min-h-screen justify-between">
                 <div>{children}</div>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
